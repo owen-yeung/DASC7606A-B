0eb31d406c1eb2245af09dc52b271aaf41424341
owen-yeung <owen_yeung@berkeley.edu>
2025-10-25 10:46:41 +0800

    add bash script to run training

---
+++ scripts/trial_train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Bash script to run the Python training script
+
+python main.py \
+  --dataset cifar100 \
+  --data_dir data/raw \
+  --output_dir results \
+  --device cuda \
+  --batch_size 128 \
+  --num_epochs 10 \
+  --lr 0.001 \
+  --weight_decay 5e-4 \
+  --num_workers 4
